a7da39bd3f22b2f20b91d856980eb391b26dc322
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-10-10 05:39:41 +0300

    Add files via upload

---
+++ Lab1.1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(0, "");
+    double num;
+    cout << "Введите число:";
+    cin >> num;
+    cout << num << "^2" << num * num << endl;
+    cout << num << "^3" << num * num * num << endl;
+    return 0;
+}
