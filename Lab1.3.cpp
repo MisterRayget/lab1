b2a14d0ded7f769049aa769400c56b0d03b77649
MisterRayget <145323958+MisterRayget@users.noreply.github.com>
2023-10-10 09:11:49 +0300

    Add files via upload

---
+++ Lab1.3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(0, "");
+    int num = 0;
+    int start;
+    int end;
+    cout << "Введите начало диапозона:";
+    cin >> start;
+    cout << "Введите конец диапозона:";
+    cin >> end;
+    for (start; start <= end; start++) {
+        if (start % 2 == 0) {
+            num += start;
+        }
+    }
+    cout << "Ответ:" << num;
+    return 0;
+}
